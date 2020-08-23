987abc3df6bb1474937605e8a4457486b0a5f8b6
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-08-23 23:08:45 +0900

    finish the Program
    finish the Program

---
+++ ytmetadata.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+
+
+try:
+    val = input("Enter your URL: ")
+    #yt_obj = YouTube('https://www.youtube.com/watch?v=d9U3w-2TI5o&list=PLtfRFndo6b7KC3jUBHDyLw3w2VqxzcYtm') 
+    yt_obj = YouTube(str(val))
+    print(f'Video Title is {yt_obj.title}')
+    print(f'Video Length is {yt_obj.length} seconds')
+    print("---------------------------------------------")
+    print(f'Video Description is \n {yt_obj.description}')
+    print("---------------------------------------------")
+    print(f'Video Rating is {yt_obj.rating}')
+    print(f'Video Views Count is {yt_obj.views}')
+    print(f'Video Author is {yt_obj.author}')
+except Exception as e:
+    print(e)
+
